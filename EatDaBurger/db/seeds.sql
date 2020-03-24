5e3855afb362a1d76420ee3b2fdccd25bec1a050
Walter Hoerman <whoerman@gmail.com>
2020-03-24 14:00:33 -0400

    formatting

--- EatDaBurger/db/seeds.sql
+++ EatDaBurger/db/seeds.sql
@@ -1,6 +1,14 @@
-INSERT INTO burgers (name) VALUES ('All-American - Beef burger with American cheese, lettuce and tomato', true);
-INSERT INTO burgers (name) VALUES ('Plains - Bison Burger with cheddar, lettuce and siracha', true);
-INSERT INTO burgers (name, eaten) VALUES ('Boring - Beef burger with ketchup', true);
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('All-American');
+INSERT INTO burgers (name) VALUES ('Up in Smoke');
+INSERT INTO burgers (name, eaten) VALUES ('OO-Mommy', true);
 INSERT INTO burgers (name, eaten) VALUES ('The Brazen', true);
 INSERT INTO burgers (name, eaten) VALUES ('Sweet Home Avacado', true);
-INSERT INTO burgers (name) VALUES ('Off the Hook');
+INSERT INTO burgers (name) VALUES ('Getting figgy with it');
+INSERT INTO burgers (name) VALUES ('The Four Horsemen');
+INSERT INTO burgers (name) VALUES ('Mac Daddy');
+INSERT INTO burgers (name, eaten) VALUES ('Not-Cho Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('The Patty Melt', true);
+INSERT INTO burgers (name, eaten) VALUES ('The Nashville Hot', true);
+INSERT INTO burgers (name) VALUES ('Earth Burger');
