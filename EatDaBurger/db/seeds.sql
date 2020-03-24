7cd437160981699610b8acb4c46a270003273963
Walter Hoerman <whoerman@gmail.com>
2020-03-24 13:07:41 -0400

    working on buttons

--- EatDaBurger/db/seeds.sql
+++ EatDaBurger/db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('The BRGR');
-INSERT INTO burgers (name) VALUES ('Up in Smoke');
-INSERT INTO burgers (name, eaten) VALUES ('OO-Mommy', true);
+INSERT INTO burgers (name) VALUES ('All-American - Beef burger with American cheese, lettuce and tomato', true);
+INSERT INTO burgers (name) VALUES ('Plains - Bison Burger with cheddar, lettuce and siracha', true);
+INSERT INTO burgers (name, eaten) VALUES ('Boring - Beef burger with ketchup', true);
 INSERT INTO burgers (name, eaten) VALUES ('The Brazen', true);
 INSERT INTO burgers (name, eaten) VALUES ('Sweet Home Avacado', true);
 INSERT INTO burgers (name) VALUES ('Off the Hook');
