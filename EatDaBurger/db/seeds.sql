02000fd5146135be7dd49b53dfdf6c603e8e8930
Walter Hoerman <whoerman@gmail.com>
2020-03-24 14:20:53 -0400

    adding images

--- EatDaBurger/db/seeds.sql
+++ EatDaBurger/db/seeds.sql
@@ -1,14 +1,14 @@
 USE burgers_db;
 
-INSERT INTO burgers (name) VALUES ('All-American');
-INSERT INTO burgers (name) VALUES ('Up in Smoke');
-INSERT INTO burgers (name, eaten) VALUES ('OO-Mommy', true);
-INSERT INTO burgers (name, eaten) VALUES ('The Brazen', true);
-INSERT INTO burgers (name, eaten) VALUES ('Sweet Home Avacado', true);
-INSERT INTO burgers (name) VALUES ('Getting figgy with it');
-INSERT INTO burgers (name) VALUES ('The Four Horsemen');
-INSERT INTO burgers (name) VALUES ('Mac Daddy');
-INSERT INTO burgers (name, eaten) VALUES ('Not-Cho Burger', true);
-INSERT INTO burgers (name, eaten) VALUES ('The Patty Melt', true);
-INSERT INTO burgers (name, eaten) VALUES ('The Nashville Hot', true);
-INSERT INTO burgers (name) VALUES ('Earth Burger');
+INSERT INTO burgers (name) VALUES ('THE BRGR - two smashed mff patties, bacon, american cheese, l.t.o, pickles, secret sauce');
+INSERT INTO burgers (name) VALUES ('OO-MOMMY - mff beef patty topped with gorgonzola, beer battered onion rings, lettuce, bacon & onion jam');
+INSERT INTO burgers (name, eaten) VALUES ('THE BRAZEN - mff beef patty topped w/ pulled pork, cheddar, fried egg, sketch, lettuce, tomato ', true);
+INSERT INTO burgers (name, eaten) VALUES ('UP IN SMOKE - bison burger, smoked gouda, applewood smoked bacon, caramelized onion, avocado, bbq, lettuce, & tomato', true);
+INSERT INTO burgers (name, eaten) VALUES ('THE NASHVILLE HOT - vermont raised spicy fried chicken, mayo, house-made pickles, & coleslaw ', true);
+INSERT INTO burgers (name) VALUES ('SWEET HOME AVOCADO - turkey patty, cheddar cheese, avocado, bacon, ancho aioli, lettuce, tomato');
+INSERT INTO burgers (name) VALUES ('NOT-CHO BRGR - mff beef patty, ancho aioli, lettuce, tomato & onion relish, jalapenos, cheese sauce, avocado, tortilla strips');
+INSERT INTO burgers (name) VALUES ('OFF THE HOOK - blackened tuna, cilantro aioli, avocado, cucumber ginger slaw');
+INSERT INTO burgers (name, eaten) VALUES ('ETTING FIGGY W/ IT - mff beef patty, smoked fig & apple jam, brie, arugula ', true);
+INSERT INTO burgers (name, eaten) VALUES ('EARTH BURGER - fried quinoa & lentil veggie patty, sautéed mushrooms, arugula walnut pesto, lettuce, & tomato served on a gruyere crusted bun', true);
+INSERT INTO burgers (name, eaten) VALUES ('THE FOUR HORSEMEN - mff beef patty, poblano salsa, jalapenos, ancho aioli, crumbled gorgonzola, lettuce, tomato', true);
+INSERT INTO burgers (name) VALUES ('PORTWALK POUTINE - mff beef patty, house fries, cheese curds, gravy');
